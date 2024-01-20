f89cb40bd725efe9619b73edec4262d9c171d820
unknown <anirbanroychowdhury10@gmail.com>
2024-01-20 22:57:13 +1100

    small fixes and QOL

--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@ video can be completely automated.
 
 The steps would be:
 1. Get the top X number of posts form story subreddit such as r/nosleep or r/aita. 
+2. Also get an image of the entire post as that will be overlayed on top of the background video - selenium script????
 2. Get the text from this post.
 3. Generate a text to speach ai voice over of the post.
 4. Generate generic music/video for background.
--- src/scrape_reddit.py
+++ src/scrape_reddit.py
@@ -1,27 +1,32 @@
 import pandas as pd
-
-# Replace these with your own credentials
-CLIENT_ID = "glClH8o6JFlouyk-kVFa6Q"
-CLIENT_SECRET = "wH4tzU6ikYc7Ci5gKHqb1q7Sce2K3g"
-USER_AGENT = "YOUR_USER_AGENT"
+from datetime import datetime
+import praw
+from bs4 import BeautifulSoup
+from markdown import markdown
+import os
+import logging
+import csv
 
 
 def init_reddit_client():
-    import praw
     reddit_client = praw.Reddit(
         client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT
     )
     return reddit_client
 
 
-def extract_top_posts(reddit_client, subreddit: str, time_filter: str) -> pd.DataFrame:
+def extract_top_posts(reddit_client: praw.Reddit , subreddit: str, limit: int, time_filter='day') -> pd.DataFrame:
+    # TODO: There are multiple problems with this fuction.
+    # 1. If a post contains only link a pic, it will not have any body to get the text from. We need to make sure we disregards post which are only made of links and images. - Put a condition to check if selfText is null
+    # 2. Praw returns us markdown text not string text. Therefore there are a lot of special charecters that we need to handle and sanitize. - ?????
+    # BUG: Write now if you open the file generated as a csv it will have fucked up formatting. This needs to be taken care off.
     posts_data = []
     print(f"Extracting Posts\n")
     try:
-        top_posts = reddit_client.subreddit(subreddit).top(limit=10)
+        top_posts = reddit_client.subreddit(subreddit).top(limit=limit)
     except Exception as e:
         logging.error("Couldn't scrape posts", exc_info=True)
-        
+
     for post in top_posts:
         post_details = {
             "url": post.url,
@@ -36,8 +41,6 @@ def extract_top_posts(reddit_client, subreddit: str, time_filter: str) -> pd.Dat
 
 # Converts markdown text to plain text
 def tranform_posts(df: pd.DataFrame) -> pd.DataFrame:
-    from bs4 import BeautifulSoup
-    from markdown import markdown
     df["body_text"] = df["body_text"].apply(
         lambda text: "".join(
             BeautifulSoup(markdown(text), features="html.parser").findAll(string=True)
@@ -45,33 +48,41 @@ def tranform_posts(df: pd.DataFrame) -> pd.DataFrame:
     )
     return df
 
-def create_folder_dir(filename: str)-> None:
-    import os
+
+def create_folder_dir(filename: str) -> None:
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+
 
 def main():
     # Init variables
     reddit_client = init_reddit_client()
-    subreddits = ["nosleep","worldnews"]
+    subreddits = ["nosleep", "amitheasshole"]
     time_filter = "day"
 
     # Get Extracts
     subreddit_extracts_list = {}
     for subreddit in subreddits:
-        scraped_post_df = extract_top_posts(reddit_client, subreddit, time_filter)
+        scraped_post_df = extract_top_posts(reddit_client, subreddit, 2, time_filter)
         scraped_post_df = tranform_posts(scraped_post_df)
         subreddit_extracts_list[subreddit] = scraped_post_df
 
     # Get Current Date
-    from datetime import datetime
     current_date = datetime.utcnow()
     # Write files as csv into a folder for safe keeping
     for subreddit_key, subreddit_extract_df in subreddit_extracts_list.items():
-        path = f"./dataset/{current_date.strftime('%Y')}/{current_date.strftime('%m')}/{current_date.strftime('%d')}/{subreddit_key}/top_extract.csv"
+        path = f"./dataset/{current_date.strftime('%Y')}/{current_date.strftime('%m')}/{current_date.strftime('%d')}/{subreddit_key}/{subreddit_key}_{current_date.strftime('%Y-%m-%d')}_extract.csv"
         create_folder_dir(path)
-        subreddit_extract_df.to_csv(path, sep='\t', encoding='utf-8')
+        subreddit_extract_df.to_csv(path, encoding="utf-8", sep=',', quoting=csv.QUOTE_NONNUMERIC, index=False)
+
 
 if __name__ == "__main__":
-    import logging
-    logging.basicConfig(filename='./logs/app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+    # Replace these with your own credentials
+    CLIENT_ID = "glClH8o6JFlouyk-kVFa6Q"
+    CLIENT_SECRET = "wH4tzU6ikYc7Ci5gKHqb1q7Sce2K3g"
+    USER_AGENT = "YOUR_USER_AGENT"
+    logging.basicConfig(
+        filename="./logs/app.log",
+        filemode="w",
+        format="%(name)s - %(levelname)s - %(message)s",
+    )
     main()
