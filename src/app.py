1c6fad7366b174d8ffb090d136366b5a905e74a7
anirban <anirbanroychowdhury10@gmail.com>
2024-01-25 14:57:51 +1100

    removing dead code

--- src/app.py
+++ src/app.py
@@ -30,33 +30,3 @@ def hello_world():
     for i in app.url_map.iter_rules():
         routes.append(i)
     return render_template("index.html", routes=routes)
-
-
-# @app.route("/extract", methods=["GET"])
-# def extract_top_post_from_subreddit():
-#     # INPUTS
-#     reddit_client = init_reddit_client(
-#         app.config["CLIENT_ID"], app.config["CLIENT_SECRET"], app.config["USER_AGENT"]
-#     )
-#     subreddits = ["nosleep", "amitheasshole"]
-#     time_filter = "day"
-
-#     subreddit_extracts_list = {}
-#     for subreddit in subreddits:
-#         scraped_post_df = extract_top_posts(reddit_client, subreddit, 2, time_filter)
-#         scraped_post_df = tranform_posts(scraped_post_df)
-#         subreddit_extracts_list[subreddit] = scraped_post_df
-
-#     # Get Current Date
-#     current_date = datetime.utcnow()
-#     # Write files as csv into a folder for safe keeping
-#     for subreddit_key, subreddit_extract_df in subreddit_extracts_list.items():
-#         path = f"./dataset/{current_date.strftime('%Y')}/{current_date.strftime('%m')}/{current_date.strftime('%d')}/{subreddit_key}/{subreddit_key}_{current_date.strftime('%Y-%m-%d')}_extract.csv"
-#         create_folder_dir(path)
-#         subreddit_extract_df.to_csv(
-#             path, encoding="utf-8", sep=",", quoting=csv.QUOTE_NONNUMERIC, index=False
-#         )
-#         conn = sqlite3.connect("reddit.db")
-#         subreddit_extract_df.to_sql("top_posts", conn, if_exists="append")
-
-#     return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
